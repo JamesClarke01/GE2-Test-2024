b49d21a62640d617c54fa4d6390861df63e90ac9
James <jamesclarkedublin@gmail.com>
2024-04-23 09:50:01 +0100

    Fields Added

--- GE2-Test-Godot/behaviors/FPController.gd
+++ GE2-Test-Godot/behaviors/FPController.gd
@@ -1,5 +1,4 @@
 extends Node3D
-#Initial commit
 
 # Declare member variables here. Examples:
 # var a = 2
---
+++ GE2-Test-Godot/exam/creature_generator.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var length: float = 20
+@export var frequency: float = 1
+@export var startAngle: float = 0
+@export var baseSize: float = 1
+@export var multiplier: float = 5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
